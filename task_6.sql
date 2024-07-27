27d12ce6cffcf9400de1041322aa12aebf2f09ca
willvm-09 <williammutugi7@gmail.com>
2024-07-27 13:02:22 +0300

    Add more data to customer table

---
+++ task_6.sql
@@ -0,0 +1,7 @@
+USE alx_book_store;
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES(1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave."),
+(2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave."),
+(3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave."),
+(4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave.");
