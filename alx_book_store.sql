9fa9d8123fd25fb9d58087970a4d876bf8f6f0d3
willvm-09 <williammutugi7@gmail.com>
2024-07-27 07:47:17 +0300

    Amend CREATE TABLE statements

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 USE alx_book_store;
 
-CREATE TABLE Books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT PRIMARY KEY,
     title VARCHAR(130),
     FOREIGN KEY (author_id) REFERENCES Authors (author_id),
@@ -10,25 +10,25 @@ CREATE TABLE Books (
     Publication_date DATE
 );
 
-CREATE TABLE Authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 )
 
-CREATE TABLE Customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 )
 
-CREATE TABLE Orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY,
     FOREIGN KEY (customer_id) REFERENCES Customers (customer_id),
     order_date DATE 
 )
 
-CREATE Order_Details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     order_detail_id INT PRIMARY KEY, 
     FOREIGN KEY (order_id) REFERENCES Orders (order_id),
     FOREIGN KEY (book_id) REFERENCES Books (book_id),
