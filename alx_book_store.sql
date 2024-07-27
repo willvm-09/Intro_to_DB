a46b9113e10bad06172c24d694216da88b8e46cf
willvm-09 <williammutugi7@gmail.com>
2024-07-27 07:49:28 +0300

    Amend FOREIGN KEY in Orders Table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS Customers (
 
 CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY,
-    FOREIGN KEY (customer_id) REFERENCES Customers (customer_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE 
 )
 
